82cfff1ba630be58af4c13eec030ae1186ef32e9
Avaer Kazmer <a@modules.io>
2022-01-16 22:44:18 -0500

    Do not attach canvas

--- index.js
+++ index.js
@@ -825,7 +825,7 @@ export default () => {
         canvas.width = size;
         canvas.height = size;
         canvas.style = `position: fixed; top: ${canvasIndex2*1024}px; left: 0; width: 1024px; height: 1024px; z-index: 10;`;
-        document.body.appendChild(canvas);
+        // document.body.appendChild(canvas);
         const ctx = canvas.getContext('2d');
         // document.body.appendChild(canvas);
         const tex = new THREE.Texture(canvas);
